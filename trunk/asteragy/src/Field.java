e7d94af6d736a8832ba402214cbc4eb2d5650981
valp_ab@yahoo.co.jp <valp_ab@yahoo.co.jp@05ab3c7c-673e-0410-85c5-735cf5110427>
2007-11-08 10:45:39 +0000

    骨だけ。骨組みではない。

---
+++ trunk/asteragy/src/Field.java
@@ -0,0 +1,4 @@
+
+class Field {
+	private Aster[][] field;
+}
---
+++ trunk/asteragy/src/KeyInputPlayer.java
@@ -0,0 +1,13 @@
+
+public class KeyInputPlayer extends Player {
+
+	/**
+	 * @param playerName プレイヤーの名前
+	 */
+	public KeyInputPlayer(String playerName) {
+		super(playerName);
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+
+}
