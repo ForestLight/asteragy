f9b05e9a26e0e806b59d25fc3db5387dd036a480
yusuke.ichinohe <yusuke.ichinohe@05ab3c7c-673e-0410-85c5-735cf5110427>
2008-05-12 15:55:02 +0000

    getRangeの削除し漏れ

--- trunk/asteragy/src/jp/ac/tokyo_ct/asteragy/SunClass.java
+++ trunk/asteragy/src/jp/ac/tokyo_ct/asteragy/SunClass.java
@@ -19,47 +19,4 @@ final class SunClass extends AsterClass {
 	int getNumber() {
 		return 1;
 	}
-
-	int[][] getRange() {
-		switch (mode) {
-		case 0:
-			return swapGetRange(sunRange);
-		case 1:
-			int[][] range = new int[sunRange.length][sunRange[0].length];
-			final Point thisPoint = getPoint();
-			// レンジの左上の座標のフィールド内での位置
-			Point pt = new Point();
-			pt.x = thisPoint.x - (range[0].length / 2);
-			pt.y = thisPoint.y - (range.length / 2);
-
-			for (int i = 0; i < sunRange.length; i++) {
-				if (!field.isYInFieldBound(pt.y + i))
-					continue;
-				for (int j = 0; j < sunRange[0].length; j++) {
-					if (!field.isXInFieldBound(pt.x + j))
-						continue;
-
-					// レンジ内であり
-					if (sunRange[i][j] == 1) {
-						range[i][j] = 1;
-						// その位置のアステルにクラスがあり
-						final Aster a = field.at(pt.y + i, pt.x + j);
-						final AsterClass c = a.getAsterClass();
-						if (c != null) {
-							// そのクラスの所持者が相手である場合選択不可能
-							if (c.getPlayer() != getPlayer()) {
-								range[i][j] = 0;
-							}
-							// サンである場合選択不可能
-							if (c.getNumber() == 1) {
-								range[i][j] = 0;
-							}
-						}
-					}
-				}
-			}
-			return range;
-		}
-		return null;
-	}
 }
